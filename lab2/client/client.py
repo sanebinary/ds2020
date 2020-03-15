9634326825435db739156dc9c45c2c12c26e97ca
sanebinary <anh.nguyenduc259wk@gmail.com>
2020-03-15 15:46:16 -0400

    add functions for server and calls for clients

--- lab2/client/client.py
+++ lab2/client/client.py
@@ -6,7 +6,7 @@ import os
 
 ############################################PARSING ARGUMENTS##############################################
 parser = argparse.ArgumentParser(description="Python RPC client")
-subparsers = parser.add_subparsers(required= True, help='subcommands', dest='subparsers')
+subparsers = parser.add_subparsers(required=True,help='subcommands', dest='subparsers')
 
 # create the parser for the "setFileName" command
 parser_a = subparsers.add_parser('setFileName', help='change filename')
@@ -15,13 +15,14 @@ parser_a.add_argument('-o', metavar='<OUTPUTNAME>', type=str, help='new file nam
 
 # create the parser for the "appendContent" command
 parser_b = subparsers.add_parser('appendContent', help='append content to a file on server')
-parser_b.add_argument('-i', type=str, required= True, help='get a file on server')
-parser_b.add_argument('-a', type=str, required= True, help='local file content to append to server file')
+parser_b.add_argument('-src', type=str, required= True, help='a local file to append data to server file')
+parser_b.add_argument('-dest', type=str, required= True, help='server file to append to')
 
 # create the parser for the "setCreationTime" command
 parser_c = subparsers.add_parser('setCreationTime', help='add creation time to file')
+parser_c.add_argument('-file', required = True, type = str, help="filename to set creation time for")
 parser_c.add_argument('-manual', type = str, help='add creation time manually')
-parser_c.add_argument('-auto', type = str, help="let's the server get time")
+parser_c.add_argument('-auto', help="let's the server get time", action='store_true')
 
 # Print help if no commands are provided
 if len(sys.argv)==1:
@@ -35,26 +36,41 @@ args = parser.parse_args()
 if (args.subparsers == 'setCreationTime'):
     if (args.manual is None and args.auto is None):
         parser.error('No argument requested, add -manual or -auto')
-    elif ((args.manual != None) and (args.auto != None)):
-        parser.error('setCreationTime only supports one argument.')
+    elif ((args.manual != None) and (args.auto != False)):
+        parser.error('setCreationTime only supports either -manual or -auto.')
 
 ############################################################################################################
-'''
-with xmlrpc.client.ServerProxy("http://localhost:12345/") as server:
-    #upload files
-    if sysarg[1] != 'setFileName':
-        setFileName()
 
-    else: 
-        server.setFileName(sys.argv[2])
-        server.setCreationTime(fileName,datetime.now())
-        with open(sys.argv[3], 'rb') as f:
-            data = f.read(1048576)
-            while (data != ''):
-                server.appendContent(fileName, data)
-            f.close()
-    
-    
+#Read 1mb of data each time
+#1 MB = 1024KB and 1 KB = 1024B so 1 MB has 1024 x 1024 = 1,048,576 bytes. That's 1,048,576 characters
+def bytes_from_file(filename, chunksize=1048576):
+    with open(filename, "r") as f:
+        while True:
+            chunk = f.read(chunksize)
+            if chunk:
+                yield from data
+            else:
+                break
+
+with xmlrpc.client.ServerProxy("http://localhost:12345/") as server:
+    #setFileName
+    if (args.subparsers == 'setFileName'):
+        print(server.setFileName(args.i,args.o))
+        exit(1)
+
+    #appendContent from local file to server
+    elif (args.subparsers == 'appendContent'):
+        for data in bytes_from_file(args.src):
+            print(server.appendContent(args.dest,data))
+
+    #setCreationTime:
+    elif (args.subparsers == 'setCreationTime'):
+        if (args.manual):
+            print(server.setCreationTime(args.file, args.manual))    
+        if (args.auto):
+            timestampStr = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            print(server.setCreationTime(args.file, timestampStr))
+
+    elif (args.subparsers == '')
     #download from server
-    exit(1)
-'''
+#    exit(1)
--- lab2/server/server.py
+++ lab2/server/server.py
@@ -1,38 +1,41 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
-
+import os
 
 def connect():
-    print('Serving RPC-FTP on localhost port 12345')
+    print('Serving RPC-FTP on localhost port 12345:')
     return (
     '''
     ********** RPC SERVICE FOR FTP **********
     =========================================
+    Ctrl + C to escape
     ''')
 
 
-def setFileName(filename):
-    open(filename, "r")
-    print("File " + filename + " has been created.")
+def setFileName(src,dest):
+    src = os.path.join(os.getcwd(),src)
+    dest = os.path.join(os.getcwd(),dest)
+    os.rename(src,dest)
+    return('File ' + src + ' has been renamed to ' + dest)
 
-
-def appendContent(filename, data):
-    with open(filename, 'a') as f:
+def appendContent(dest, data):
+    with open(dest, 'a') as f:
         f.write(data)
-        print("File has been created")
+        return(dest + ' has been appended with contents from local file')
 
 def setCreationTime(filename,createdTime):
     # In Unix systems, file has no creation time attribute (or it exists as some others debate but difficult to manually set it):
     # https://unix.stackexchange.com/questions/118577/changing-a-files-date-created-and-last-modified-attributes-to-another-file 
-    # So I will just insert the date on top of the files :)
-    with open(filename, 'wb') as f:
-        f.write(createdTime)
-        print("File is created at " + createdTime)
+    # So I will just append the time to the file
+    
+    with open(filename, 'a') as f:
+        f.write("//Creation Date: " + createdTime)
+        return(filename + " is created at " + createdTime)
 
 
-with SimpleXMLRPCServer(("localhost", 12345), logRequests=False) as server:
+with SimpleXMLRPCServer(("localhost", 12345), logRequests=False, allow_none=True) as server:
     #upload files
-    server.register_introspection_functions()
+    server.register_function(connect, "connect")
     server.register_function(setFileName, "setFileName")
     server.register_function(appendContent, "appendContent")
     server.register_function(setCreationTime, "setCreationTime")
